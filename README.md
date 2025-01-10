8591d81a385aeab6744fc72a1be0b489246e8c9c
Arianna Mihu <mih21002@byui.edu>
2025-01-10 13:55:39 -0700

    NPS 1

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ Use this as a starting point to complete the WDD 231 personal activity. We will 
 ## Other commands
 
 - `npm run build` to build final files when you are ready to turn in.
+
+## Site Production
+
+This site can be viewed at: https://incomparable-malasada-1e8a5a.netlify.app/
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+const first_img_link = parkData.images[0].url;
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="park-header__title">${info.name}</a>
+    <p class="park-header__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
+
+ document.getElementsByClassName(header-img).src = first_img_link;
+ document.querySelector(".park-header__subtitle").innerHTML = parkData.info.states;
